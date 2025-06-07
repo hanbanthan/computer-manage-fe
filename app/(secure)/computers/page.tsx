aa58341111fd6b8dbfd1d9c8cd51c434f9922178
giapnguyen <giapnguyen2004@gmail.com>
2025-06-07 07:53:33 +0700

    fixed relationship of user and computer

--- app/(secure)/computers/page.tsx
+++ app/(secure)/computers/page.tsx
@@ -7,21 +7,6 @@ import useUserService from "@/app/_services/useUserService";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-interface Computer {
-    computer_id: string;
-    name?: string;
-    cpu?: string;
-    ram?: string;
-    ssd?: string;
-    hdd?: string;
-    room?: string;
-    building?: string;
-    user?: {
-        id: string;
-        username: string;
-    }
-}
-
 export default function Computers() {
     const [query, SetQuery] = useState("");
     const { token } = useAuth();
@@ -76,7 +61,7 @@ export default function Computers() {
                                     <td>{computer.room}</td>
                                     <td>{computer.building}</td>
                                     <td style={{ whiteSpace: 'nowrap' }}>
-                                        {(computer.user?.username === currentUser?.username ||
+                                        {(
                                             currentUser?.role === 'admin' ||
                                             currentUser?.role === 'superadmin') && (
                                                 <>
@@ -124,36 +109,36 @@ export default function Computers() {
         }
     }
 
-return (
-    <>
-        <h1>Computers</h1>
-        <Link href="/computers/add" className="btn btn-sm btn-success mb-2">Add Computer</Link>
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th style={{ width: '13%' }}>name</th>
-                    <th style={{ width: '13%' }}>cpu</th>
-                    <th style={{ width: '13%' }}>ram</th>
-                    <th style={{ width: '13%' }}>ssd</th>
-                    <th style={{ width: '13%' }}>hdd</th>
-                    <th style={{ width: '13%' }}>room</th>
-                    <th style={{ width: '13%' }}>building</th>
-                    <th style={{ width: '9%' }}>
-                        <div style={{ display: 'flex' }}>
-                            🔎 <input
-                                type="text"
-                                placeholder="Search Computers"
-                                value={query}
-                                onChange={(event) => updateQuery(event.target.value)}
-                            />
-                        </div>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <TableBody />
-            </tbody>
-        </table>
-    </>
-);
+    return (
+        <>
+            <h1>Computers</h1>
+            <Link href="/computers/add" className="btn btn-sm btn-success mb-2">Add Computer</Link>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th style={{ width: '13%' }}>name</th>
+                        <th style={{ width: '13%' }}>cpu</th>
+                        <th style={{ width: '13%' }}>ram</th>
+                        <th style={{ width: '13%' }}>ssd</th>
+                        <th style={{ width: '13%' }}>hdd</th>
+                        <th style={{ width: '13%' }}>room</th>
+                        <th style={{ width: '13%' }}>building</th>
+                        <th style={{ width: '9%' }}>
+                            <div style={{ display: 'flex' }}>
+                                🔎 <input
+                                    type="text"
+                                    placeholder="Search Computers"
+                                    value={query}
+                                    onChange={(event) => updateQuery(event.target.value)}
+                                />
+                            </div>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <TableBody />
+                </tbody>
+            </table>
+        </>
+    );
 }
