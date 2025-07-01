d77eac7599dba186299a3318397037edb8059ef1
giapnguyen <giapnguyen2004@gmail.com>
2025-07-01 20:27:42 +0700

    minor

--- app/(secure)/layout.tsx
+++ app/(secure)/layout.tsx
@@ -1,14 +1,14 @@
 // import { redirect } from "next/navigation";
 //import { cookies, headers } from "next/headers";
 import { cookies } from "next/headers";
-import { auth } from "../_helpers/server/auth";
+// import { auth } from "../_helpers/server/auth";
 import Nav from "../_components/nav";
 import Alert from "../_components/alert";
 import React from "react";
 import { AuthProvider } from "../_context/auth-context";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-    const isAuthenticated = await auth.isAuthenticated();
+    // const isAuthenticated = await auth.isAuthenticated();
     const cookieStore = cookies();
     const accessToken = (await cookieStore).get("authorization")?.value ?? null;
     // if (!isAuthenticated) {
