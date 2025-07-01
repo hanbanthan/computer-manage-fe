faa18e4a701f1c827ae0d491e8a897b36765b84b
giapnguyen <giapnguyen2004@gmail.com>
2025-07-02 00:47:03 +0700

    minor

--- app/(secure)/users/layout.tsx
+++ app/(secure)/users/layout.tsx
@@ -1,16 +1,16 @@
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
-import { auth } from "@/app/_helpers/server/auth";
-import { redirect } from "next/navigation";
+// import { auth } from "@/app/_helpers/server/auth";
+// import { redirect } from "next/navigation";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-    console.log("🔒 Auth layout is running...");
-    const isAllowed = await auth.hasRole(['admin', 'superadmin']);
+    // console.log("🔒 Auth layout is running...");
+    // const isAllowed = await auth.hasRole(['admin', 'superadmin']);
 
-    if (!isAllowed) {
-        console.log("🚫 Not allowed. Redirecting...");
-        redirect('/unauthorized');
-    }
+    // if (!isAllowed) {
+    //     console.log("🚫 Not allowed. Redirecting...");
+    //     redirect('/unauthorized');
+    // }
 
     return <>{children}</>;
 }
